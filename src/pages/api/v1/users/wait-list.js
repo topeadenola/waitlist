c626cdc4b755a8fa8a467239e0eba76444eca884
David Adebayo <davidadebayo702@gmail.com>
2024-07-02 12:56:29 +0100

    sendgrid

--- src/pages/api/v1/users/wait-list.js
+++ src/pages/api/v1/users/wait-list.js
@@ -1,5 +1,8 @@
 // src/pages/api/v1/users/wait-list.js
 import { MongoClient } from 'mongodb';
+import sgMail from '@sendgrid/mail';
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const handler = async (req, res) => {
   if (req.method !== 'POST') {
@@ -22,10 +25,25 @@ const handler = async (req, res) => {
       return res.status(400).json({ message: 'Email is required' });
     }
 
+    // Save the email to the database
     await collection.insertOne({ email, timestamp: new Date() });
-    res.status(201).json({ message: 'You have been added to the waitlist.' });
+
+    // Define the email content and template data
+    const msg = {
+      to: email,
+      from: 'ubreedlearn@gmail.com',
+      templateId: 'd-0088a80ef1a34b58b5e578d303633911',
+      dynamic_template_data: {
+        // Populate dynamic data as required
+      },
+    };
+
+    // Send the email
+    await sgMail.send(msg);
+
+    res.status(201).json({ message: 'You have been added to the waitlist and an email has been sent.' });
   } catch (error) {
-    console.error('Error connecting to the database:', error);
+    console.error('Error connecting to the database or sending email:', error);
     res.status(500).json({ message: 'Internal server error', error });
   } finally {
     await client.close();
@@ -33,3 +51,8 @@ const handler = async (req, res) => {
 };
 
 export default handler;
+
+
+
+
+
