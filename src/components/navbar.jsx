92d1142733770aabc624215a669b5a6a4bdedff1
David Adebayo <davidadebayo702@gmail.com>
2024-09-23 01:22:10 +0100

    done, maybe

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Navbar = ({scrollToAbout, scrollToCareer, scrollToContact}) => {
   return (
     <div className=" bg-[#00230F] flex">
-      <div className="flex text-sm md:text-normal gap-6 mx-auto w-full max-w-6xl px-4 py-6 items-center justify-end">
-        <div  onClick={() => scrollToAbout()}>about us .</div>
-        <div onClick={()=>scrollToCareer()} >career .</div>
-        <div onClick={scrollToContact}>contact .</div>
+      <div className="flex text-sm md:text-base  gap-6 mx-auto w-full max-w-6xl px-4 py-6 items-center justify-end">
+        <div className="hover:text-[#D0EA50] cursor-pointer" onClick={() => scrollToAbout()}>about us .</div>
+        <div className="hover:text-[#D0EA50] cursor-pointer" onClick={()=>scrollToCareer()} >career .</div>
+        <div className="hover:text-[#D0EA50] cursor-pointer" onClick={scrollToContact}>contact .</div>
       </div>
     </div>
   );
